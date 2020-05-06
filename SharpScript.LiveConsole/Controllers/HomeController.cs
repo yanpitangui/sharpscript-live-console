67f112e1ad74ea6b8a561ac6adb348fa289a9a24
yanpitangui <yanpitangui@gmail.com>
2020-05-06 17:51:17 -0300

    commit inicial

---
+++ SharpScript.LiveConsole/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceStack.Script;
+
+namespace SharpScript.LiveConsole.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ScriptContext _scriptContext;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _scriptContext = new ScriptContext().Init();
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/linq/eval")]
+        public async Task<IActionResult> Eval(string script)
+        {
+            try
+            {
+                return Ok(await _scriptContext.RenderScriptAsync(script ?? ""));
+
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ SharpScript.LiveConsole/wwwroot/js/site.js
@@ -0,0 +1,59 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(".linq-preview").each(function () {
+
+    var el = $(this);
+    el.find("textarea").on("input", function () {
+        var text = el.find(".template textarea").val();
+        $.ajax({
+            cache: false,
+            type: "POST",
+            url: "/linq/eval",
+            data: { script: text }
+        }).done(function (data) {
+            el.removeClass('error').find(".output").html(data);
+        }).fail(function (jqxhr) { handleError(el, jqxhr) })
+    })
+        .trigger("input")
+
+})
+
+$("#content h2,#content h3,#content h4").each(function () {
+    var el = $(this);
+
+    var text = el.html();
+    if (text.indexOf("<") >= 0) return;
+
+    if (!el.attr('id')) {
+        var safeName = text.toLowerCase().replace(/\s+/g, "-").replace(/[^a-zA-Z0-9_-]+/g, "")
+        el.attr('id', safeName)
+    }
+
+    el.on('click', function () {
+        var id = el.attr('id')
+        location.href = "#" + id
+    })
+})
+
+
+function handleError(el, jqxhr) {
+    try {
+        el.addClass('error')
+        var errorResponse = jqxhr.responseText;
+        el.find('.output').html('<div class="alert alert-danger"><pre>' + errorResponse +'</pre></div>')
+    } catch (e) {
+        el.find('.output').html('<div class="alert alert-danger"><pre>' + jqxhr.status + ' ' + jqxhr.statusText + '</pre></div>')
+    }
+}
+
+function queryStringParams(qs) {
+    qs = (qs || document.location.search).split('+').join(' ')
+    var params = {}, tokens, re = /[?&]?([^=]+)=([^&]*)/g
+    while (tokens = re.exec(qs)) {
+        params[tokens[1]] = tokens[2];
+    }
+    return params;
+}
